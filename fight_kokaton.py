4f63f6a0447f36bfc6b124a8ec2c5fdeed146392
C0A23092 <c0a23092cf@edu.teu.ac.jp>
2024-06-25 17:08:01 +0900

    演習課題1

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -2,6 +2,7 @@ import os
 import random
 import sys
 import time
+from turtle import Screen
 import pygame as pg
 
 
@@ -141,6 +142,18 @@ class Bomb:
         screen.blit(self.img, self.rct)
 
 
+class Score:
+    def __init__(self):
+        self.fonto = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 30)
+        score_num = 0
+        self.img = self.fonto.render("スコア：0", 0, (0, 0, 255))
+
+    def update(self, score_num, screen: pg.Surface):
+        self.img = self.fonto.render("スコア：{}".format(score_num), 0, (0, 0, 255))
+        screen.blit(self.img, (100, HEIGHT-50))
+
+
+
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
@@ -149,8 +162,10 @@ def main():
     beam = None
     # bomb = Bomb((255, 0, 0), 10)
     bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
+    score = Score()
     clock = pg.time.Clock()
     tmr = 0
+    score_num = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -177,6 +192,7 @@ def main():
                     bombs[i] = None
                     beam = None
                     bird.change_img(6, screen)
+                    score_num += 1
         bombs = [bomb for bomb in bombs if bomb is not None]
 
         key_lst = pg.key.get_pressed()
@@ -185,7 +201,9 @@ def main():
             beam.update(screen) 
         for bomb in bombs:
             bomb.update(screen)
+        score.update(score_num, screen)
         pg.display.update()
+        
         tmr += 1
         clock.tick(50)
 
